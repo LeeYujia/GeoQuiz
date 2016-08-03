6516a70254af8f7587772b2e8159cb3d4d886eab
zhouf <zhouf_t@sohu.com>
2016-08-03 15:49:37 +0800

    提交测试，描述修改了什么内容

--- app/src/main/java/com/example/msi/geoquiz/IntroActivity.java
+++ app/src/main/java/com/example/msi/geoquiz/IntroActivity.java
@@ -8,6 +8,6 @@ public class IntroActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.introduction);
         //abcde
-        //11111111111111111111111
+        //11111111111111111
     }
 }
