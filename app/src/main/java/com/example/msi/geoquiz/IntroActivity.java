f8c85013b2701867b40970b36879c141bdcae369
LeeYujia <LeeYujia@gmail.com>
2016-08-03 17:35:55 +0800

    1 skip

--- app/src/main/java/com/example/msi/geoquiz/IntroActivity.java
+++ app/src/main/java/com/example/msi/geoquiz/IntroActivity.java
@@ -2,11 +2,20 @@ package com.example.msi.geoquiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class IntroActivity extends Activity{
+    private Button back;
     protected void onCreat(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.introduction);
+        back =(Button)findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+            }
+        });
 
     }
 }
