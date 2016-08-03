bd628ce8ab59bb7ff0d64b611e38b6ea3f906e19
whole9681@gmail.com <lanhou199681>
2016-08-03 20:48:42 +0800

    welcomeView/test

--- app/src/main/java/com/example/msi/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/msi/geoquiz/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.msi.geoquiz;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
